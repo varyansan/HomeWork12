f056c51cd289b21f3f7586b68476e10d3d218ff0
varyansan <varyan.san.98@gmail.com>
2024-06-21 16:04:40 +0300

    Homework is done. Classes and objects are created.

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    String title;
+    int yearOfPublication;
+    private Author author;
+
+    public Book(String title, int yearOfPublication, Author author) {
+        this.title = title;
+        this.yearOfPublication = yearOfPublication;
+
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Author kurtVonnegut = new Author("Курт", "Воннегут");
+        Author kanaeMinato = new Author("Канаэ", "Минато");
+
+        Book catsCradle = new Book("Колыбель для кошки", 2001, kurtVonnegut);
+        Book guilty = new Book("Виновен", 2022, kanaeMinato);
+        guilty.setYearOfPublication(2015);
+
+        System.out.println("Первая книга: " + "\"" + catsCradle.title + "\"" + ", написанна в " + catsCradle.yearOfPublication
+                + " году. Автор: " + kurtVonnegut.firstName + " " + kurtVonnegut.secondName);
+        System.out.println("Вторая книга: " + "\"" + guilty.title + "\"" + ", написанна в " + guilty.yearOfPublication +
+                " году. Автор: " + kanaeMinato.firstName + " " + kanaeMinato.secondName);
+
 
     }
 }
